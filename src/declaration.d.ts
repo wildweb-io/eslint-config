4ae56726ac32a354a5525a50a0a9e456925ac13b
pnodet <paul.nodet@gmail.com>
2023-11-03 02:18:43 +0700

    refactor: clean up

---
+++ src/declaration.d.ts
@@ -0,0 +1,12 @@
+declare module 'eslint-config-prettier';
+declare module 'eslint-plugin-antfu';
+declare module 'eslint-plugin-eslint-comments';
+declare module 'eslint-plugin-i';
+declare module 'eslint-plugin-markdown';
+declare module 'eslint-plugin-sort-keys';
+declare module 'eslint-plugin-unicorn';
+declare module 'eslint-plugin-unused-imports';
+declare module 'eslint-plugin-react-hooks';
+declare module 'eslint-plugin-react';
+declare module 'eslint-plugin-jsx-a11y';
+declare module '@next/eslint-plugin-next';
--- src/plugins.ts
+++ src/plugins.ts
@@ -1,67 +1,46 @@
-/* eslint-disable import/newline-after-import */
-/* eslint-disable import/order */
-/* eslint-disable import/first */
-/* eslint-disable func-style */
 /* eslint-disable no-restricted-syntax */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* #__NO_SIDE_EFFECTS__ */
-function interopDefault(mod: any) {
-	return mod.default || mod;
-}
-
-import * as _pluginPrettier from 'eslint-plugin-prettier';
-export const pluginPrettier = interopDefault(_pluginPrettier);
-
-// @ts-expect-error missing types
 import * as _configPrettier from 'eslint-config-prettier';
-export const configPrettier = interopDefault(_configPrettier);
-
-import * as _pluginTypeScript from '@typescript-eslint/eslint-plugin';
-export const pluginTypeScript = interopDefault(_pluginTypeScript);
-
 import * as _pluginAntfu from 'eslint-plugin-antfu';
-export const pluginAntfu = interopDefault(_pluginAntfu);
-
-export * as parserTypeScript from '@typescript-eslint/parser';
-
-// @ts-expect-error missing types
-export * as pluginUnusedImports from 'eslint-plugin-unused-imports';
-
-// @ts-expect-error missing types
 import * as _pluginComments from 'eslint-plugin-eslint-comments';
-export const pluginComments = interopDefault(_pluginComments);
-
-// @ts-expect-error missing types
-export * as pluginImport from 'eslint-plugin-i';
-
-// @ts-expect-error missing types
-import * as _pluginUnicorn from 'eslint-plugin-unicorn';
-export const pluginUnicorn = interopDefault(_pluginUnicorn);
-
-// @ts-expect-error missing types
+import * as _pluginImport from 'eslint-plugin-i';
+import * as _pluginJsonc from 'eslint-plugin-jsonc';
 import * as _pluginMarkdown from 'eslint-plugin-markdown';
+import * as _pluginPrettier from 'eslint-plugin-prettier';
+import * as _pluginSortKeys from 'eslint-plugin-sort-keys';
+import * as _pluginTypeScript from '@typescript-eslint/eslint-plugin';
+import * as _pluginUnicorn from 'eslint-plugin-unicorn';
+import * as _pluginUnusedImports from 'eslint-plugin-unused-imports';
+import * as _pluginYml from 'eslint-plugin-yml';
+import * as _reactHooksPlugin from 'eslint-plugin-react-hooks';
+import * as _reactPlugin from 'eslint-plugin-react';
+import * as _a11yPlugin from 'eslint-plugin-jsx-a11y';
+import * as _graphqlESLint from '@graphql-eslint/eslint-plugin';
+import * as _nextjsPlugin from '@next/eslint-plugin-next';
+import * as _parserJsonc from 'jsonc-eslint-parser';
+import * as _parserYml from 'yaml-eslint-parser';
+import * as _parserTypeScript from '@typescript-eslint/parser';
+
+const interopDefault = (mod: any): any => {
+	return mod.default || mod;
+};
+
+export const configPrettier = interopDefault(_configPrettier);
+export const pluginAntfu = interopDefault(_pluginAntfu);
+export const pluginComments = interopDefault(_pluginComments);
+export const pluginImport = interopDefault(_pluginImport);
+export const pluginJsonc = interopDefault(_pluginJsonc);
 export const pluginMarkdown = interopDefault(_pluginMarkdown);
-
-// @ts-expect-error missing types
-export * as pluginSortKeys from 'eslint-plugin-sort-keys';
-
-// @ts-expect-error missing types
-export * as pluginYml from 'eslint-plugin-yml';
-
-// @ts-expect-error missing types
-export * as reactPlugin from 'eslint-plugin-react';
-
-// @ts-expect-error missing types
-export * as reactHooksPlugin from 'eslint-plugin-react-hooks';
-
-// @ts-expect-error missing types
-export * as a11yPlugin from 'eslint-plugin-jsx-a11y';
-
-// @ts-expect-error missing types
-export * as nextjsPlugin from '@next/eslint-plugin-next';
-
-export * as parserJsonc from 'jsonc-eslint-parser';
-export * as pluginJsonc from 'eslint-plugin-jsonc';
-export * as parserYml from 'yaml-eslint-parser';
-
-export * as graphqlESLint from '@graphql-eslint/eslint-plugin';
+export const pluginPrettier = interopDefault(_pluginPrettier);
+export const pluginSortKeys = interopDefault(_pluginSortKeys);
+export const pluginTypeScript = interopDefault(_pluginTypeScript);
+export const pluginUnicorn = interopDefault(_pluginUnicorn);
+export const pluginUnusedImports = interopDefault(_pluginUnusedImports);
+export const pluginYml = interopDefault(_pluginYml);
+export const reactHooksPlugin = interopDefault(_reactHooksPlugin);
+export const reactPlugin = interopDefault(_reactPlugin);
+export const a11yPlugin = interopDefault(_a11yPlugin);
+export const graphqlESLint = interopDefault(_graphqlESLint);
+export const nextjsPlugin = interopDefault(_nextjsPlugin);
+export const parserJsonc = interopDefault(_parserJsonc);
+export const parserYml = interopDefault(_parserYml);
+export const parserTypeScript = interopDefault(_parserTypeScript);
